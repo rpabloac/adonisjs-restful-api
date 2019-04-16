099e2d6726f7cc9740b8f5944d1da62ba54ab96b
rpabloac <rpabloac@gmail.com>
2019-04-16 00:20:02 -0300

    Add files via upload

---
+++ start/routes.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.0/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.group(() => {
+  Route.post('books', 'BookController.store')
+  Route.get('books', 'BookController.index')
+  Route.get('books/:id', 'BookController.show')
+  Route.put('books/:id', 'BookController.update')
+  Route.delete('books/:id', 'BookController.delete')
+}).prefix('api/v1')
